f2a4306f2828118959e0cdcca89d91c3d0d4638e
Francisco <franciscoocontatoo@gmail.com>
2025-12-08 23:35:07 -0300

    Rewriting Class Product

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Market.Models
 {
-    internal class Produto
+    public class Produto
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Categoria { get; set; }
-        public string Marca { get; set; }
-        public string ?Descricao { get; set; }
+        public string Nome { get; set; } = string.Empty; // It is used to avoid null reference issues
+        public string Categoria { get; set; } = string.Empty;
+        public string Marca { get; set; } = string.Empty;
+        public string? Descricao { get; set; }
         public decimal PrecoUnitario { get; set; }
     }
 }
