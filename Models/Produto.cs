ad3ed2a51554694bda195f29a7e066ba60348ca4
Francisco <franciscoocontatoo@gmail.com>
2025-12-08 23:31:38 -0300

    Model created

---
+++ Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Models
+{
+    internal class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public string Marca { get; set; }
+        public string ?Descricao { get; set; }
+        public decimal PrecoUnitario { get; set; }
+    }
+}
